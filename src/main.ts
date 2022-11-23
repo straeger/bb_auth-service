b50b79b50ce372139c16c6145e7feeab4e57ccbe
Stephan Traeger <Stephan.Traeger@hannover-re.com>
2022-11-23 09:18:41 +0100

    more init stuff

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { Logger } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice({
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 4000,
+    },
+  });
+
+  //await app.startAllMicroservicesAsync();
+  await app.listen(3000);
+  Logger.log('Auth microservice running');
+}
+bootstrap();
